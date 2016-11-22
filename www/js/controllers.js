0a37b8bdba04b0dd00f765f4a15168f929c45f47
Victor Aluko <victor.aluko@gmail.com>
2016-11-22 13:09:33 +0200

    Fixed comments based on PR

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,7 +38,6 @@ angular.module('taksi_driver.controllers', [])
   $scope.registerData = {};
 
   $scope.doRegister = function () {
-    console.log('Simulating Register');
 
     // Registration would be done here
     $timeout(function() {
@@ -52,7 +51,6 @@ angular.module('taksi_driver.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
