2be083b75d3f082f308fd117a13eb7b93e66943a
Victor Aluko <victor.aluko@gmail.com>
2016-11-29 12:22:38 +0200

    created spec test for registration controller

--- www/test/spec/register_controller_spec.js
+++ www/test/spec/register_controller_spec.js
@@ -10,14 +10,20 @@ describe('Registration Test Suite', function () {
 
   beforeEach(module('taxi_home_driver'));
 
-  var RegisterCtrl, scope;
+  var RegisterCtrl, scope, $httpBackend = {};
 
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_) {
     scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_;
     RegisterCtrl = $controller('RegisterCtrl', {
       $scope: scope
     });
   }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
   it('should bind to the field data', function () {
     expect(scope.registerData).toBeDefined();
@@ -25,7 +31,18 @@ describe('Registration Test Suite', function () {
 
   });
 
-  xit('should define all fields of registration', function () {
-    expect(scope.registerData.first_name).toBeDefined();
+  it('should submit a request to the backend service', function () {
+    $httpBackend
+      .expectPOST('http://localhost:3000/api/users')
+      .respond(201);
+    scope.registerData = {
+      first_name: 'Victor', last_name: 'Aluko',
+      dob: '2016-06-06', car_color: 'red', car_type: 'mercedes',
+      plate_number: 'qqqqqq', email_address: 'victor@local.com',
+      password: 'qwerty', password_confirmation: 'qwerty'
+    };
+    scope.doRegister();
+    $httpBackend.flush();
+    expect(scope.syncNotification).toEqual('Booking is being processed');
   });
 });
